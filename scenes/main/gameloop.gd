4c7fd5a138efd85ead5ebd5e88966818c791a4c8
whotookXetheon <106356261+whotookXetheon@users.noreply.github.com>
2025-02-09 10:29:45 -0500

    mostly finished

--- scenes/main/gameloop.gd
+++ scenes/main/gameloop.gd
@@ -1,8 +1,15 @@
 extends Node3D
 
 @onready var group_of_wegas: Node3D = $"group of wegas"
+@onready var label: Label = $GUI/Label
+@onready var sfx: AudioStreamPlayer = $SFX
+
+@onready var wegasleft = group_of_wegas.get_child_count()
 
 func _process(delta: float) -> void:
-	var wegasleft = group_of_wegas.get_child_count()
+	if wegasleft != group_of_wegas.get_child_count():
+		sfx.play()
+	wegasleft = group_of_wegas.get_child_count()
+	label.text = "wegas left: %s" % wegasleft
 	if wegasleft == 0:
 		get_tree().change_scene_to_file("res://scenes/main/main2.tscn")
--- scenes/main/gameloop2.gd
+++ scenes/main/gameloop2.gd
@@ -1,8 +1,10 @@
 extends Node3D
 
 @onready var group_of_wegas: Node3D = $"group of wegas"
+@onready var label: Label = $GUI/Label
 
 func _process(delta: float) -> void:
 	var wegasleft = group_of_wegas.get_child_count()
+	label.text = "wegas left: %s" % wegasleft
 	if wegasleft == 0:
 		get_tree().change_scene_to_file("res://scenes/menus/win/win.tscn")
