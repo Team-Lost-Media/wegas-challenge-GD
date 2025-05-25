7714d78baa8dc5229b61761a8b8c5e58a13a8837
somerandomguy21 <migliojac@gmail.com>
2025-05-25 22:10:08 +0200

    added wip high score
    im missing an option to reset it
    also planning on adding an option to adjust your starting angle

---
+++ scenes/autoload/save.gd
@@ -0,0 +1,12 @@
+extends Node
+
+const SAVE_SCORE_FILE_PATH = "res://high_score.wegakill"
+
+func save_score(score):
+	var file = FileAccess.open(SAVE_SCORE_FILE_PATH, FileAccess.WRITE)
+	file.store_string(str(score))
+
+func load_score():
+	var file = FileAccess.open(SAVE_SCORE_FILE_PATH, FileAccess.READ)
+	var score = int(file.get_as_text())
+	return score
--- scenes/menus/win/win.gd
+++ scenes/menus/win/win.gd
@@ -7,8 +7,11 @@ extends Node2D
 @onready var time: Label = $VBoxContainer/time
 @onready var points: Label = $VBoxContainer/points
 @onready var final_score: Label = $"VBoxContainer/final score number"
+@onready var high_score_label: Label = $"VBoxContainer/new high score"
 @export var points_and_time_curve: Curve
 @export var tutorial = false
+
+var high_score: bool
 
 func _ready() -> void:
 	if tutorial == false:
@@ -24,9 +27,20 @@ func _ready() -> void:
 		print("score multiplier = ", score_multiplier)
 		#actually display it
 		final_score.text = str(snapped(score, 1))
+		#save it and show off if it's a high score
+		if Save.load_score() < snapped(score, 1):
+			Save.save_score(snapped(score, 1))
+			high_score_label.label_settings.font_color = Color(1, 0, 0)
+			high_score = true
 
 func _process(delta: float) -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	if high_score == true:
+		high_score_label.label_settings.font_color.h += 0.2 * delta
+		high_score_label.text = "NEW HIGH SCORE!"
+	else:
+		high_score_label.text = str("previous high score: ", Save.load_score())
+	
 	if tutorial == false:
 		if Input.is_action_just_pressed("jump") and timer.is_stopped():
 			get_tree().change_scene_to_file("res://scenes/menus/main/mainMenu.tscn")
