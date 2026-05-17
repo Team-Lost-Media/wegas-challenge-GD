83e637ac8b3720e2cee75922af48a270182bbf5c
somerandomguy021 <233308715+somerandomguy021@users.noreply.github.com>
2026-05-17 12:29:48 +0200

    fixed the reset save data thing

--- scenes/menus/main/delete everything/delete_everything.gd
+++ scenes/menus/main/delete everything/delete_everything.gd
@@ -2,7 +2,7 @@ extends Control
 
 func _on_reset_button_pressed() -> void:
 	Save.reset_everything()
-	get_tree().change_scene_to_file("res://scenes/menus/main/mainMenu.tscn")
+	get_tree().quit()
 
 
 func _on_quit_button_pressed() -> void:
