bc7b2df9fb27b770610790ece5cee60bf670d931
somerandomguy021 <233308715+somerandomguy021@users.noreply.github.com>
2026-06-12 18:00:12 +0200

    fixed the theidol line not showing up on first launch

--- scenes/menus/main/orange_line.gd
+++ scenes/menus/main/orange_line.gd
@@ -4,6 +4,9 @@ var green_line: bool = Save.progress_dict.get("beat wcti")
 var orange_line: bool = Save.progress_dict.get("beat wcti orange line")
 
 func _ready() -> void:
+	await get_tree().process_frame
+	green_line = Save.progress_dict.get("beat wcti")
+	orange_line = Save.progress_dict.get("beat wcti orange line")
 	if orange_line:
 		show()
 		color = Color.ORANGE
